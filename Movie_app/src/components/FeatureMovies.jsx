21c6c787a150a9254c8735c174cc18025d794a65
MisanToghanro <tmisan657@gmail.com>
2024-09-27 13:20:49 +0100

    add new changes

--- Movie_app/src/components/FeatureMovies.jsx
+++ Movie_app/src/components/FeatureMovies.jsx
@@ -4,6 +4,7 @@ import fetchFeatureMovieApi from "../ApiComponent/featureMoviesApi" // api for f
 const FeatureMovies = () => {
 
     const[featureMovies , setFeatureMovies] = useState([]);
+    const [dropDown , setDropDown] = useState(null);
     const[error , setError] = useState("")
 
  
@@ -19,6 +20,10 @@ const FeatureMovies = () => {
 
   useEffect(() => {getMovies()} ,[]);
 
+  const openDropDown = (id) => {
+    setDropDown(dropDown === id ? null : id)
+  }
+
     return(
         <div className="mt-10 p-5">
             <h2 className="text-red-500 font-bold mb-5 text-center"> Popular Movies:</h2>
@@ -28,10 +33,19 @@ const FeatureMovies = () => {
          <div className="m-5 mb-  grid gap-6  md:grid-cols-3 sm:grid-cols-2 ">
          {featureMovies.map(movie => ( 
             <div key={movie.id} className="max-w-sm shadow-lg shadow-red-400 p-4   rounded-lg  bg-gray-800 "> 
-             <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-            <h3 className="text-white font-bold bg-red-500 p-4 mb-2 rounded-md">Title:{movie.title}</h3>
-            <p className="text-white font-bold bg-red-500 p-4 mb-2 rounded-md">About:{movie.overview}</p>
-            <p className="text-white font-bold bg-red-500 p-4 mb-2 rounded-md">Rating:{movie.vote_average}</p>
+             <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className="mb-2"/>
+            <h3 className="text-red-500 font-bold   mb-2 ">Title:{movie.title}</h3>
+            <p className="text-red-500 mb-2"><strong>Release Date:</strong> {movie.release_date}</p>
+
+            <button onClick={() => openDropDown(movie.id)} className="text-red-500">
+                 {dropDown === movie.id ? "Hide details" : "Show details"}
+            </button>
+
+            {dropDown === movie.id && ( <div>
+                <p className="text-white font-bold bg-red-500 p-4 mb-2 rounded-md">About:{movie.overview}</p>
+                <p className="text-white font-bold bg-red-500 p-4 mb-2 rounded-md">Rating:{movie.vote_average}</p>
+            </div>)}
+
             </div>
         ))}
          </div>
